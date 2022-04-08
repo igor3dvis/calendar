46e8a5c178df6ae8c78b11cfe47dd787e2e9501d
igor3dvis <80128927+igor3dvis@users.noreply.github.com>
2022-04-08 03:00:10 +0300

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import * as ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App"
-import reportWebVitals from "./reportWebVitals";
+import App from "./App";
 
-ReactDOM.render(
+const container = document.getElementById("root");
+
+const root = ReactDOM.createRoot(container);
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+    <App tab="home" />
+  </React.StrictMode>
 );
-
-reportWebVitals();
